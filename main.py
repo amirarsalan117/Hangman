84ed91f22b053f3016a855fef8076077b80ebd0e
amirarsalan117 <122307820+amirarsalan117@users.noreply.github.com>
2023-01-11 11:52:58 +0000

    get user input, if the guess letter is correct we'll replace it with '-' in answar list

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 words = ['apple', 'pie', 'salam']
 
 chosen_word = random.choice(words)
-
+print(f'the chosen word is :{chosen_word}')
 #Step2: declare a list that contain - for every letter in chosen word
 answer_list = []
 
@@ -12,3 +12,11 @@ for letter in chosen_word:
     answer_list.append('-')
     
 
+#Step3: Get User input and if the guess is true replace it with the answar list  selected position
+user_input = input("Enter a letter: ")
+for i in range(len(answer_list)):
+    if user_input == chosen_word[i]:
+        del answer_list[i]
+        answer_list.insert(i,user_input)
+            
+print(answer_list)
